20e97c2ffb732ff9e0af842b038afcc1e734c023
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-11-27 18:35:22 +0100

    comments + todo

--- src/Library/DetectionConfidence.cs
+++ src/Library/DetectionConfidence.cs
@@ -39,21 +39,12 @@
 namespace Ude
 {
     /// <summary>
-    /// Indicate how confident the detection module about the return result.
-    ///     
-    ///  NoAnswerYet: the detector have not find out a answer yet based on 
-    ///  the data it received.
+    /// TODO not used?
     /// 
-    ///  BestAnswer: the answer the detector returned is the best one within 
-    ///     the knowledge of the detector. In other words, the test to all 
-    ///     other candidates fail.
-    ///     For example, the (Shift_JIS/EUC-JP/ISO-2022-JP) detection
-    ///     module may return this with answer "Shift_JIS " if it receive 
-    ///     bytes > 0x80 (which make ISO-2022-JP test failed) and byte 
-    ///     0x82 (which may EUC-JP test failed)
-    ///
-    ///  SureAnswer: the detector is 100% sure about the  answer.
-    ///  
+    /// 
+    /// Indicate how confident the detection module about the return result.
+    /// </summary>
+    /// <example>
     ///  Example 1: the Shift_JIS/ISO-2022-JP/EUC-JP detector return
     ///    this w/ ISO-2022-JP when it hit one of the following ESC seq
     ///     ESC ( J
@@ -64,12 +55,30 @@ namespace Ude
     ///     when the first 2 byte are BOM mark.
     ///  Example 3: the Korean detector return ISO-2022-KR when it
     ///     hit ESC $ ) C
-    /// </summary>
+    /// </example>
     public enum DetectionConfidence
     {
+        /// <summary>
+        /// The detector have not find out a answer yet based on  the data it received.
+        /// </summary>
         NoAnswerYet = 0,
+
+        /// <summary>
+        /// The answer the detector returned is the best one within 
+        /// the knowledge of the detector. In other words, the test to all 
+        /// other candidates fail.
+        /// For example, the (Shift_JIS/EUC-JP/ISO-2022-JP) detection
+        /// module may return this with answer "Shift_JIS " if it receive 
+        /// bytes > 0x80 (which make ISO-2022-JP test failed) and byte 
+        /// 0x82 (which may EUC-JP test failed)
+        /// </summary>
         BestAnswer,
+
+        /// <summary>
+        /// The detector is 100% sure about the  answer.
+        /// </summary>
         SureAnswer,
+
         NoAnswerMatch
     }
 }
