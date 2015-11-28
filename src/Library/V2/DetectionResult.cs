da142a0ade27dde377fac8b24b5f7e3edc087466
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-11-28 13:10:01 +0100

    new result objects

---
+++ src/Library/V2/DetectionResult.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using Ude.Core;
+
+namespace Ude.V2
+{
+    public class DetectionResult
+    {
+        private readonly string _encodingShortName;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public DetectionResult(string encodingShortName, float confidence, CharsetProber prober, TimeSpan? time)
+        {
+            _encodingShortName = encodingShortName;
+            Confidence = confidence;
+
+            try
+            {
+                Encoding = System.Text.Encoding.GetEncoding(encodingShortName);
+            }
+            catch (Exception)
+            {
+                
+               //wrong name
+            }
+         
+            Prober = prober;
+            Time = time;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public DetectionResult(CharsetProber prober, TimeSpan? time) 
+            : this(prober.GetCharsetName(), prober.GetConfidence(), prober, time)
+        {
+        }
+
+        [Obsolete("v1")]
+        public string Charset
+        {
+            get { return _encodingShortName; }
+        }
+
+        public Encoding Encoding { get; set; }
+
+        public float Confidence { get; set; }
+
+        public CharsetProber Prober { get; set; }
+
+        public TimeSpan? Time { get; set; }
+
+    }
+}
---
+++ src/Library/V2/DetectionSummary.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ude.V2
+{
+    public class DetectionSummary
+    {
+        /// <summary>
+        /// Empty
+        /// </summary>
+        public DetectionSummary()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public DetectionSummary(IList<DetectionResult> allDetectionResults)
+        {
+            AllDetectionResults = allDetectionResults;
+        }
+
+        public DetectionSummary(DetectionResult allDetectionResults)
+        {
+            AllDetectionResults = new List<DetectionResult> { allDetectionResults };
+        }
+
+        public DetectionResult Detected
+        {
+            get { return AllDetectionResults?.FirstOrDefault(); }
+        }
+
+        public IList<DetectionResult> AllDetectionResults { set; get; }
+    }
+}
