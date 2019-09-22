f93f496356d4617f36c14f6334803e4477d49add
Rustam Sayfutdinov <rstm.sf@gmail.com>
2019-09-22 11:58:45 +0300

    resolve discussion: return public permissions

--- src/DetectionResult.cs
+++ src/DetectionResult.cs
@@ -41,7 +41,7 @@ namespace UtfUnknown
         /// <summary>
         /// All results
         /// </summary>
-        public IList<DetectionDetail> Details { get; private set; }
+        public IList<DetectionDetail> Details { get; set; }
 
         public override string ToString()
         {
