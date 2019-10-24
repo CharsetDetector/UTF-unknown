2c5f5a6705b27a6b3ae776fb4df692e8302a4c96
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-10-24 21:09:20 +0200

    Support .net core 2 and .net standard 2

---
+++ tests/EncodingJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UtfUnknown.Tests
+{
+    public class EncodingJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(Encoding).IsAssignableFrom(objectType);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((Encoding)value).WebName);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return Encoding.GetEncoding((string)reader.Value);
+        }
+    }
+}
